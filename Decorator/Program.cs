05d4a5d44e05ed29a1ef5574ad1f5c8f9d845ae1
Fauly Coelho <faulybsb@gmail.com>
2023-09-19 19:49:29 -0300

    create project

---
+++ Decorator/Program.cs
@@ -0,0 +1,133 @@
+﻿
+
+ICoffee coffee = new SimpleCoffee();
+Console.WriteLine($"Description: {coffee.GetDescription()}");
+Console.WriteLine($"Cost: ${coffee.GetCost()}");
+
+// Add Milk, Sugar, and Cream to the coffee
+coffee = new MilkDecorator(coffee);
+coffee = new SugarDecorator(coffee);
+coffee = new CreamDecorator(coffee);
+Console.WriteLine($"Description: {coffee.GetDescription()}");
+Console.WriteLine($"Cost: ${coffee.GetCost()}");
+
+// Add Whipped Cream and Cinnamon to the coffee
+coffee = new WhippedCreamDecorator(coffee);
+coffee = new CinnamonDecorator(coffee);
+Console.WriteLine($"Description: {coffee.GetDescription()}");
+Console.WriteLine($"Cost: ${coffee.GetCost()}");
+
+
+
+public interface ICoffee
+{
+    string GetDescription();
+    double GetCost();
+}
+
+public class SimpleCoffee : ICoffee
+{
+    public string GetDescription()
+    {
+        return "Simple Coffee";
+    }
+
+    public double GetCost()
+    {
+        return 2.0;
+    }
+}
+
+public abstract class CoffeeDecorator : ICoffee
+{
+    private ICoffee _coffee;
+
+    public CoffeeDecorator(ICoffee coffee)
+    {
+        _coffee = coffee;
+    }
+
+    public virtual string GetDescription()
+    {
+        return _coffee.GetDescription();
+    }
+
+    public virtual double GetCost()
+    {
+        return _coffee.GetCost();
+    }
+}
+
+public class MilkDecorator : CoffeeDecorator
+{
+    public MilkDecorator(ICoffee coffee) : base(coffee) { }
+
+    public override string GetDescription()
+    {
+        return base.GetDescription() + ", with Milk";
+    }
+
+    public override double GetCost()
+    {
+        return base.GetCost() + 1.0;
+    }
+}
+
+public class SugarDecorator : CoffeeDecorator
+{
+    public SugarDecorator(ICoffee coffee) : base(coffee) { }
+
+    public override string GetDescription()
+    {
+        return base.GetDescription() + ", with Sugar";
+    }
+
+    public override double GetCost()
+    {
+        return base.GetCost() + 0.5;
+    }
+}
+public class CreamDecorator : CoffeeDecorator
+{
+    public CreamDecorator(ICoffee coffee) : base(coffee) { }
+
+    public override string GetDescription()
+    {
+        return base.GetDescription() + ", with Cream";
+    }
+
+    public override double GetCost()
+    {
+        return base.GetCost() + 0.75;
+    }
+}
+
+public class WhippedCreamDecorator : CoffeeDecorator
+{
+    public WhippedCreamDecorator(ICoffee coffee) : base(coffee) { }
+
+    public override string GetDescription()
+    {
+        return base.GetDescription() + ", with Whipped Cream";
+    }
+
+    public override double GetCost()
+    {
+        return base.GetCost() + 1.25;
+    }
+}
+
+public class CinnamonDecorator : CoffeeDecorator
+{
+    public CinnamonDecorator(ICoffee coffee) : base(coffee) { }
+
+    public override string GetDescription()
+    {
+        return base.GetDescription() + ", with Cinnamon";
+    }
+
+    public override double GetCost()
+    {
+        return base.GetCost() + 0.5;
+    }
+}
---
+++ Strategy/Program.cs
@@ -0,0 +1,62 @@
+﻿var cart = new ShoppingCart();
+
+cart.SetPaymentStrategy(new CreditCardPayment("1234-5678-9012-3456", "Fauly Coelho"));
+cart.Checkout(100);
+
+cart.SetPaymentStrategy(new PayPalPayment("fauly.coelho@example.com"));
+cart.Checkout(50);
+
+
+public interface IPaymentStrategy
+{
+    void Pay(int amount);
+}
+
+
+public class CreditCardPayment : IPaymentStrategy
+{
+    private string cardNumber;
+    private string name;
+
+    public CreditCardPayment(string cardNumber, string name)
+    {
+        this.cardNumber = cardNumber;
+        this.name = name;
+    }
+
+    public void Pay(int amount)
+    {
+        Console.WriteLine($"Paid ${amount} with Credit Card ({cardNumber})");
+    }
+}
+
+public class PayPalPayment : IPaymentStrategy
+{
+    private string email;
+
+    public PayPalPayment(string email)
+    {
+        this.email = email;
+    }
+
+    public void Pay(int amount)
+    {
+        Console.WriteLine($"Paid ${amount} with PayPal ({email})");
+    }
+}
+
+// Step 3: Create the Context class
+public class ShoppingCart
+{
+    private IPaymentStrategy paymentStrategy;
+
+    public void SetPaymentStrategy(IPaymentStrategy paymentStrategy)
+    {
+        this.paymentStrategy = paymentStrategy;
+    }
+
+    public void Checkout(int amount)
+    {
+        paymentStrategy.Pay(amount);
+    }
+}
